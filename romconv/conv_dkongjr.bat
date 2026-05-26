25a2ee855498d957a328127d60b189c8c7f079b7
speckhoiler <dinodark@t-online.de>
2026-05-26 20:23:01 +0200

    ROM converter fix2

--- romconv/conv_dkongjr.bat
+++ romconv/conv_dkongjr.bat
@@ -11,21 +11,21 @@ if errorlevel 1 goto :error
 cd dkongjr
 
 echo Donkey Kong Junior CPU code
-cpu_conv.py
-sound_conv.py
+python ./cpu_conv.py
+python ./sound_conv.py
 if errorlevel 1 goto :error
 
 echo Donkey Kong Junior Tiles
-tilemap_conv.py
-view_tiles_graphic.py
+python ./tilemap_conv.py
+python ./view_tiles_graphic.py
 if errorlevel 1 goto :error
 
 echo Donkey Kong Junior Colormaps
-cmap_conv.py
+python ./cmap_conv.py
 if errorlevel 1 goto :error
 
 echo Donkey Kong Junior Sprites
-sprites_conv.py
+python ./sprites_conv.py
 if errorlevel 1 goto :error
 
 echo --- Success ---
